395b318b9ce4c5d5633a09da6b6bc48a697b777b
juan <juanjrodriguez083@gmail.com>
2026-05-13 20:26:50 -0500

    correccion de error en deploy vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['192.168.2.35'],
-  },
 };
 
 export default nextConfig;
